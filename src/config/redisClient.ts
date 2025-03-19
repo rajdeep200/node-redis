8c5d5f7abafb41ed9c927ca0c0095ee7331b42d7
Rajdeep Ghosh <rajdeep.ghosh@fedev.cbnits.com>
2025-03-19 12:09:32 +0530

    third commit

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 const client = createClient({
-    url: "redis://localhost:6379"
+    url: process.env.REDIS_URL
 });
 
 client.on("error", (err) => console.error("❌ Redis Error:", err));
